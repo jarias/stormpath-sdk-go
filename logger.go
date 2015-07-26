bf2a4bfe29b52615301bd0857fd75f47042e145f
Julio Arias <jarias01@gmail.com>
2015-07-25 20:01:45 -0600

    Remove global var

--- logger.go
+++ logger.go
@@ -7,18 +7,18 @@ import (
 	"github.com/hashicorp/logutils"
 )
 
+//Logger library wide logger
 var Logger *log.Logger
-var logLevel string
 
 func initLog() {
-	logLevel = os.Getenv("STORMPATH_LOG_LEVEL")
+	logLevel := os.Getenv("STORMPATH_LOG_LEVEL")
 
 	if logLevel == "" {
 		logLevel = "ERROR"
 	}
 
 	filter := &logutils.LevelFilter{
-		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
+		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR", "NONE"},
 		MinLevel: logutils.LogLevel(logLevel),
 		Writer:   os.Stderr,
 	}
