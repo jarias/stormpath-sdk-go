60c4f256ed188a1ac5a5f51ab7b761c4875c3799
Julio Arias <jarias01@gmail.com>
2015-12-17 14:15:40 -0600

    Bump version

--- stormpath.go
+++ stormpath.go
@@ -20,7 +20,7 @@ import (
 var BaseURL = "https://api.stormpath.com/v1/"
 
 //Version is the current SDK Version
-const version = "0.1.0-beta.12"
+const version = "0.1.0-beta.13"
 
 const (
 	Enabled                   = "ENABLED"
